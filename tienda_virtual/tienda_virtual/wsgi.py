a53f1481ac77d6d6648f638f8b3ce691d2631c78
JUAN VARO <juavarver@alum.us.es>
2025-12-03 20:11:38 +0100

    fix(deploy): fixes wsgi.py due to nesting

--- tienda_virtual/tienda_virtual/wsgi.py
+++ tienda_virtual/tienda_virtual/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tienda_virtual.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tienda_virtual.tienda_virtual.settings')
 
 application = get_wsgi_application()
